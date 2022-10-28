fa8ccd2bf947716d7d2a21201ddff8b256cabb98
Martín Acosta <tinchoz49@gmail.com>
2022-10-27 21:04:38 -0300

    fixed swagger

--- src/api/app.js
+++ src/api/app.js
@@ -9,14 +9,29 @@ import fastifyDatabase from './plugins/database.js'
 import User from './models/user.js'
 import Task from './models/task.js'
 
+function loadDocumentation (app, documentation) {
+  app.register(fastifySwagger, documentation)
+
+  app.register(fastifySwaggerUi, {
+    routePrefix: '/api/documentation',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest (request, reply, next) { next() },
+      preHandler (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header
+  })
+}
+
 export default function createApp (opts = {}) {
   const { logger = false, knexConfig, documentation } = opts
 
   const app = fastify({
-    logger,
-    rewriteUrl: (req) => {
-      return req.url.replace('/api', '')
-    }
+    logger
   })
 
   app.register(fastifyDatabase, {
@@ -27,24 +42,6 @@ export default function createApp (opts = {}) {
     ]
   })
 
-  if (documentation) {
-    app.register(fastifySwagger, documentation)
-
-    app.register(fastifySwaggerUi, {
-      routePrefix: '/api/documentation',
-      uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false
-      },
-      uiHooks: {
-        onRequest (request, reply, next) { next() },
-        preHandler (request, reply, next) { next() }
-      },
-      staticCSP: true,
-      transformStaticCSP: (header) => header
-    })
-  }
-
   app.register(fastifySensible)
 
   app.decorate('verifyCookie', async (request, reply) => {
@@ -53,14 +50,18 @@ export default function createApp (opts = {}) {
 
   app.register(fastifyAuth)
 
-  // success response should return wrapped in a data prop
-  app.addHook('preSerialization', (request, reply, payload, done) => {
-    const newPayload = { data: payload }
-    done(null, newPayload)
-  })
+  if (documentation) loadDocumentation(app, documentation)
 
-  app.register(import('./routes/user.js'), { prefix: 'users' })
-  app.register(import('./routes/task.js'), { prefix: 'tasks' })
+  app.register(async (app) => {
+    // success response should return wrapped in a data prop
+    app.addHook('preSerialization', (request, reply, payload, done) => {
+      const newPayload = { data: payload }
+      done(null, newPayload)
+    })
+
+    await app.register(import('./routes/user.js'), { prefix: 'users' })
+    await app.register(import('./routes/task.js'), { prefix: 'tasks' })
+  }, { prefix: 'api' })
 
   return app
 }
